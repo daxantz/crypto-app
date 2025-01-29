f76dbc3c7f984f5b18d367bbe3242531e0e53645
daxantz <daxantz@gmail.com>
2025-01-28 21:25:37 -0500

    added required stateand searching logic

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import search from "@/public/icons/search.png";
+import { useGetAllCurrenciesQuery } from "@/lib/cryptoApi";
+import { searchCoins } from "@/lib/types/searchCoin";
 const SearchInput = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [filterCoins, setFilterCoins] = useState<searchCoins[] | undefined>([]);
+  const { data, isLoading } = useGetAllCurrenciesQuery();
+  if (isLoading) return <p>Loading...</p>;
+
   return (
     <div>
       <div className="flex justify-center items-center relative rounded-md ">
@@ -16,10 +23,28 @@ const SearchInput = () => {
         />
       </div>
       <input
-        className="bg-[#CCCCFA66] dark:bg-[#191925] px-12 py-2 w-80 h-12 rounded-md "
+        className="bg-[#CCCCFA66] dark:bg-[#191925] px-12 py-2 w-[22.25rem] h-12 rounded-md "
         type="text"
         placeholder="Search..."
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+          setTimeout(() => {
+            if (searchValue === "") return;
+            setFilterCoins(() => {
+              return data?.filter((coin) => {
+                return coin.name.toLowerCase().includes(searchValue);
+              });
+            });
+          }, 1000);
+        }}
       />
+      {filterCoins !== undefined && searchValue !== "" && (
+        <div className="h-[564px] w-[22.25rem] border border-red-500 overflow-scroll z-10 absolute bg-[#FFFFFF0D] p-4">
+          {filterCoins.map((coin) => (
+            <p key={coin.id}>{coin.name}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
