c8d04226f5d45c78e434395a98b7495090c5f21f
daxantz <daxantz@gmail.com>
2025-02-01 16:06:28 -0500

    added state for the search input value and derived the filter coins state based on the query typed in by the user. Also added useMemo for caching the filter value

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,14 +1,23 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import search from "@/public/icons/search.png";
 import { useGetAllCurrenciesQuery } from "@/lib/cryptoApi";
-import { searchCoins } from "@/lib/types/searchCoin";
+
+import Link from "next/link";
+
 const SearchInput = () => {
-  const [searchValue, setSearchValue] = useState("");
-  const [filterCoins, setFilterCoins] = useState<searchCoins[] | undefined>([]);
   const { data, isLoading } = useGetAllCurrenciesQuery();
+  const [searchValue, setSearchValue] = useState("");
+
+  const filteredCoins = useMemo(() => {
+    if (searchValue === "") return;
+    return data?.filter((coin) =>
+      coin.name.toLowerCase().startsWith(searchValue.toLowerCase())
+    );
+  }, [data, searchValue]);
+
   if (isLoading) return <p>Loading...</p>;
 
   return (
@@ -24,24 +33,19 @@ const SearchInput = () => {
       </div>
       <input
         className="bg-[#CCCCFA66] dark:bg-[#191925] px-12 py-2 w-[22.25rem] h-12 rounded-md "
-        type="text"
+        type="search"
         placeholder="Search..."
         onChange={(e) => {
           setSearchValue(e.target.value);
-          setTimeout(() => {
-            if (searchValue === "") return;
-            setFilterCoins(() => {
-              return data?.filter((coin) => {
-                return coin.name.toLowerCase().includes(searchValue);
-              });
-            });
-          }, 1000);
         }}
+        value={searchValue}
       />
-      {filterCoins !== undefined && searchValue !== "" && (
-        <div className="h-[564px] w-[22.25rem] border border-red-500 overflow-scroll z-10 absolute bg-[#FFFFFF0D] p-4">
-          {filterCoins.map((coin) => (
-            <p key={coin.id}>{coin.name}</p>
+      {filteredCoins !== undefined && searchValue !== "" && (
+        <div className="max-h-[564px] w-[22.25rem] border border-red-500 overflow-scroll z-10 absolute bg-[#FFFFFF0D] p-4">
+          {filteredCoins?.map((coin) => (
+            <Link key={coin.id} href={`/${coin.id}`}>
+              <p className="hover:bg-[#ffffff47]">{coin.name}</p>
+            </Link>
           ))}
         </div>
       )}
