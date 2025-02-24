e553c484e4151440e2ad39b265d06af6902af944
daxantz <daxantz@gmail.com>
2025-02-23 22:37:44 -0500

    added syles to graph chart

--- components/Graphchart.tsx
+++ components/Graphchart.tsx
@@ -15,7 +15,9 @@ import { Line } from "react-chartjs-2";
 import { chartCoin } from "@/lib/types/chartCoin";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 import { SerializedError } from "@reduxjs/toolkit";
-// import { format } from "date-fns";
+import Humanize from "humanize-plus";
+import { format } from "date-fns";
+import { searchCoins } from "@/lib/types/searchCoin";
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -57,19 +59,21 @@ const Graphchart = ({
   coinData,
   isLoading,
   error,
+  selectedCoin,
 }: {
   coinData: chartCoin;
   isLoading: boolean;
   error: FetchBaseQueryError | SerializedError | undefined;
+  selectedCoin: searchCoins;
 }) => {
   const volume = coinData?.total_volumes.map((array) => array[1]);
 
-  //   const latestVolume =
-  //     coinData?.total_volumes[coinData.total_volumes.length - 1][1];
-  //   const latestTime =
-  //     coinData?.total_volumes[coinData.total_volumes.length - 1][0];
+  const latestVolume =
+    coinData?.total_volumes[coinData.total_volumes.length - 1][1];
+  const latestTime =
+    coinData?.total_volumes[coinData.total_volumes.length - 1][0];
 
-  //   const date = format(new Date(latestTime || Date.now()), "MMM d, yyyy");
+  const date = format(new Date(latestTime || Date.now()), "MMM d, yyyy");
   const [gradient, setGradient] = useState<CanvasGradient | null>(null);
   const chartRef = useRef<ChartJS<"line", number[], string> | null>(null);
 
@@ -83,8 +87,8 @@ const Graphchart = ({
         0,
         chartRef.current.height
       );
-      gradient.addColorStop(0, "#56218a");
-      gradient.addColorStop(1, "orange");
+
+      gradient.addColorStop(1, "rgba(116, 116, 242, 0.6)");
       setGradient(gradient);
     }
   }, [chartRef]);
@@ -106,13 +110,24 @@ const Graphchart = ({
         label: "Volume 24h",
         data: volume,
         backgroundColor: gradient || "rgba(0, 0, 0, 0.2)", // Apply gradient or fallback to a default color
-        borderRadius: 10,
+        borderWidth: 5,
+        borderColor: "#7878FA",
         fill: true,
+        pointRadius: 0,
+        pointBorderRadius: 50,
+        lineTension: 0.4,
       },
     ],
   };
   return (
     <div className="bg-[#191932] w-[50%] flex flex-col gap-6 p-6 rounded-xl">
+      <p className="text-[20px] text-[#D1D1D1]">
+        {selectedCoin.name} ({selectedCoin.symbol.toLocaleUpperCase()})
+      </p>
+      <p className="font-bold text-2xl">
+        ${Humanize.compactInteger(latestVolume || 0, 2)}
+      </p>
+      <p className="text-[#B9B9BA] ">{date}</p>
       <Line ref={chartRef} options={options} data={data} />
     </div>
   );
