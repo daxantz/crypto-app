715cd22619bd20ebc6f7c52d139a0d4343f04b99
daxantz <daxantz@gmail.com>
2025-03-02 22:52:43 -0500

    created component to display all the coins in a table format

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import CoinCarousel from "@/components/CoinCarousel";
+import CoinTable from "@/components/CoinTable";
 
 export default function Home() {
   return (
     <div>
       <CoinCarousel />
+      <CoinTable />
       <p>coins</p>
     </div>
   );
