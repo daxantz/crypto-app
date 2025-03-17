dbe0161ced52e08918c381fdc4dd14ead7d183ae
daxantz <daxantz@gmail.com>
2025-03-16 22:10:33 -0400

    created components that display the data for the current coin

--- components/CoinImageBox.tsx
+++ components/CoinImageBox.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const CoinImageBox = ({
@@ -5,17 +6,18 @@ const CoinImageBox = ({
   title,
   symbol,
 }: {
-  image: string;
-  title: string;
-  symbol: string;
+  image: string | undefined;
+  title: string | undefined;
+  symbol: string | undefined;
 }) => {
+  if (!image || !title || !symbol) return <p>not found</p>;
   return (
-    <div className="bg-[#1E1932] rounded-xl py-10 px-14  flex flex-col items-center w-[25%] gap-2">
+    <div className="bg-[#1E1932] rounded-xl py-10 px-14  flex flex-col items-center justify-center h-[265px]  gap-6">
       <div className="bg-[#2C2C4A] rounded-md p-4 text-center w-16 h-16 ">
-        {image}
+        <Image src={image} alt="coin logo" width={32} height={32} />
       </div>
-      <p className="text-2xl">
-        {title} <span>({symbol})</span>
+      <p className="text-3xl">
+        {title} <span>({symbol.toUpperCase()})</span>
       </p>
     </div>
   );
