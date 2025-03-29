78c8baef95b778d1e47af9d4d9e3fe3773f35373
daxantz <daxantz@gmail.com>
2025-03-28 21:09:15 -0400

    created input for the porfolio modal to search for coins

---
+++ components/CoinDropdown.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import AsyncSelect from "react-select/async";
+import { StylesConfig } from "react-select";
+import { selectedCoin } from "./PortfolioDialog";
+
+const customStyles: StylesConfig = {
+  control: (provided) => ({
+    ...provided,
+    backgroundColor: "#191925",
+    border: "none",
+    width: "420px",
+    padding: "8px",
+  }),
+  input: (provided) => ({
+    ...provided,
+
+    color: "white",
+  }),
+  placeholder: (provided) => ({
+    ...provided,
+    color: "white",
+  }),
+
+  menu: (provided) => ({
+    ...provided,
+    backgroundColor: "#4e4e4e",
+    borderRadius: "4px",
+    width: "420px",
+  }),
+
+  menuList: (provided) => ({
+    ...provided,
+    backgroundColor: "black",
+  }),
+
+  option: (provided, state) => ({
+    ...provided,
+    backgroundColor: state.isSelected ? "#6161D6" : "transparent",
+    color: "white",
+    cursor: "pointer",
+    padding: "10px",
+  }),
+  indicatorSeparator: (provided) => ({
+    ...provided,
+    display: "none",
+  }),
+  dropdownIndicator: (provided) => ({
+    ...provided,
+    color: "white",
+    padding: "0 10px",
+    transform: "translateY(-50%)",
+    position: "absolute",
+    right: "-10px",
+    top: "50%",
+    width: "35px",
+  }),
+};
+export type option = {
+  id: string;
+  name: string;
+  large: string;
+  symbol: string;
+};
+
+type CoinSelectProps = {
+  setSelectedCoin: React.Dispatch<React.SetStateAction<selectedCoin | null>>;
+};
+const CoinSelect = ({ setSelectedCoin }: CoinSelectProps) => {
+  const loadOptions = async (inputValue: string) => {
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/search?query=${inputValue}`
+    );
+    const data = await res.json();
+
+    const options = data.coins.map((coin: option) => ({
+      label: coin.name,
+      value: coin.name,
+      large: coin.large,
+      id: coin.id,
+      symbol: coin.symbol,
+    }));
+    return options;
+  };
+
+  const handleChange = (newValue: unknown) => {
+    setSelectedCoin(newValue as selectedCoin);
+  };
+
+  return (
+    <AsyncSelect
+      styles={customStyles}
+      cacheOptions
+      loadOptions={loadOptions}
+      defaultOptions
+      placeholder="Select Coins"
+      onChange={handleChange}
+    />
+  );
+};
+
+export default CoinSelect;
