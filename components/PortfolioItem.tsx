7f14abfbfeb168f8c77315de445b4a863c0ad52d
daxantz <daxantz@gmail.com>
2025-04-05 18:06:41 -0400

    moved data fetching for the portfolio item to its parent using a rtk query hook so the component only makes a single request to the api instead of multiple requests based on how many portfolio items there are

--- components/PortfolioItem.tsx
+++ components/PortfolioItem.tsx
@@ -1,36 +1,34 @@
-"use client";
-
-import { useGetCoinByIdQuery } from "@/lib/cryptoApi";
 import { coin } from "@/lib/portfolioSlice";
-import { RootState } from "@/lib/store";
+import { BatchedCoin } from "@/lib/types/batchedCoin";
 import Image from "next/image";
 import React from "react";
-import { useSelector } from "react-redux";
 
-const PortfolioItem = (props: coin) => {
-  const { data } = useGetCoinByIdQuery(props.id);
-  const currency = useSelector((state: RootState) => state.currency.currency);
+type portfolioItemProps = {
+  currency: string;
+  coin: coin;
+  data: BatchedCoin[];
+};
+
+const PortfolioItem = ({ currency, coin, data }: portfolioItemProps) => {
+  const foundCoin = data.find((c) => c.id === coin.id);
+
   const marketcapVsVolume =
-    data &&
-    (data?.market_data.total_volume[currency] /
-      data?.market_data.market_cap[currency]) *
-      100;
-  const circSupplyVsMaxSupply = data?.market_data?.max_supply
-    ? (data?.market_data.circulating_supply / data?.market_data?.max_supply) *
-      100
+    foundCoin && (foundCoin?.total_volume / foundCoin?.market_cap) * 100;
+  const circSupplyVsMaxSupply = foundCoin?.max_supply
+    ? (foundCoin?.circulating_supply / foundCoin.max_supply) * 100
     : null;
-  const totalValue = data?.market_data.current_price[currency];
-  const athChangePercentage = data?.market_data.ath_change_percentage[currency];
-  const hrChangePercentage = data?.market_data.price_change_percentage_24h;
+  const totalValue = foundCoin?.current_price;
+  const athChangePercentage = foundCoin?.ath_change_percentage;
+  const hrChangePercentage = foundCoin?.price_change_percentage_24h;
 
   return (
     <div className="flex rounded-lg">
       <div className="bg-[#191932] w-[23.7rem] py-6 px-4 flex flex-col gap-8 rounded-l-lg">
         <div className="flex gap-2">
-          <Image width={32} height={32} src={props.image} alt="coin image" />
+          <Image width={32} height={32} src={coin.image} alt="coin image" />
 
           <h2 className="font-bold text-2xl">
-            {props.name} ({props.symbol.toUpperCase()})
+            {coin.name} ({coin.symbol.toUpperCase()})
           </h2>
         </div>
         <div className=" flex flex-col gap-2">
@@ -48,13 +46,13 @@ const PortfolioItem = (props: coin) => {
             </span>
           </p>
           <p className="text-[#D1D1D1] text-sm">
-            Recent purchase {props.datePurchased.toString()}
+            Recent purchase {coin.datePurchased.toString()}
           </p>
         </div>
       </div>
       <div className="bg-[#2D2D51] grid grid-cols-2 flex-1 py-6 px-4 gap-5 rounded-r-xl">
         <div className=" border border-[#191932] rounded-lg px-2 py-3">
-          <p>${props.amountPurchased}</p>
+          <p>${coin.amountPurchased}</p>
           <p className="text-[#D1D1D1] text-sm">Amount purchased</p>
         </div>
         <div
