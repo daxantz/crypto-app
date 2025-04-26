c8def9fca19c65514e253961744f73ed1d1a9e5d
daxantz <daxantz@gmail.com>
2025-04-25 20:03:00 -0400

    created util function for formatting stringsand a type for exchange rate objects

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatWithDashes(
+  string: string | undefined
+): string | undefined {
+  if (!string) return;
+  //abort if string doesnt have a space
+  if (!string.includes(" ")) return string;
+  //split string with spaces into array
+  const words = string.split(" ");
+  //join words together with -
+  const stringWithDashes = words.join("-");
+  return stringWithDashes;
+}
