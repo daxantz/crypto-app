6dcd9f8b16450ed3a634b34b58eb36e2d8e80e94
daxantz <daxantz@gmail.com>
2025-04-27 12:14:18 -0400

    created 2 helper functions that fetch the prices for a coin based off the id passed in and a function for getting the average price for a given month

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-
+import { chartPrices } from "./types/chartCoin";
+import { format } from "date-fns";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -16,4 +17,50 @@ export function formatWithDashes(
   //join words together with -
   const stringWithDashes = words.join("-");
   return stringWithDashes;
+}
+
+export async function fetchCoinGraphPrices(
+  coinId: string,
+  days: string
+): Promise<[number, number][]> {
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${days}`,
+    {
+      cache: "force-cache",
+    }
+  );
+  const data = await res.json();
+  return data.prices;
+}
+
+type monthlyAverages = {
+  [month: string]: number[];
+};
+export function getMonthlyAverages(
+  prices: chartPrices[]
+): [string, number][] | undefined {
+  if (!prices) return undefined;
+  const monthlyAverages: monthlyAverages = {};
+
+  prices.forEach(([timestamp, price]) => {
+    const month = format(new Date(timestamp), "MMM");
+
+    if (!monthlyAverages[month]) {
+      monthlyAverages[month] = [];
+    }
+
+    monthlyAverages[month].push(price);
+  });
+
+  const array = Object.entries(monthlyAverages);
+  const avgArray = array.map((array) => {
+    const avg =
+      array[1].reduce((acc, curr) => {
+        return (acc += curr);
+      }, 0) / array[1].length;
+
+    return [array[0], avg];
+  });
+
+  return avgArray as [string, number][];
 }
