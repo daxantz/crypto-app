b07ccfb875469ff8a41524101ce173ea0c0e9927
daxantz <daxantz@gmail.com>
2025-06-01 18:35:29 -0400

    refactored intervalselector and pageselector components using the group toggle component from shadcn

--- components/PageSelector.tsx
+++ components/PageSelector.tsx
@@ -1,52 +1,38 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
-import { Skeleton } from "./ui/skeleton";
-import { useState } from "react";
-import { useEffect } from "react";
+
+import { ToggleGroup, ToggleGroupItem } from "./ui/toggle-group";
 
 const PageSelector = () => {
   const pathName = usePathname();
-  const { theme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  const buttons = [
-    { href: "/", label: "Coins" },
-    { href: "/convertor", label: "Convertor" },
-  ];
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-  if (!mounted) {
-    return <Skeleton className="w-[30rem] rounded-md h-[48px]" />;
-  }
 
   return (
-    <div className="hidden sm:block">
-      {buttons.map(({ href, label }) => {
-        const isActive = pathName === href;
-        const isDark = theme === "dark";
-
-        const baseClasses = "w-[15.25rem] rounded-md py-3 px-4 ";
-        const activeClass = "bg-[#6161D680] btn";
-        const inactiveDark = "bg-[#232336] text-white";
-        const inactiveLight = "bg-white text-black";
-
-        return (
-          <Link key={href} href={href}>
-            <button
-              className={`${baseClasses} ${
-                isActive ? activeClass : isDark ? inactiveDark : inactiveLight
-              }`}
-            >
-              {label}
-            </button>
-          </Link>
-        );
-      })}
+    <div className="hidden sm:block w-[31rem] ">
+      <ToggleGroup
+        className="hidden sm:block"
+        type="single"
+        defaultValue={pathName}
+      >
+        <Link href={"/"}>
+          <ToggleGroupItem
+            className="dark:data-[state=on]:bg-[#6161D680] dark:data-[state=off]:bg-[#232336] px-4 py-3 w-1/2"
+            value="/"
+          >
+            Coins
+          </ToggleGroupItem>
+        </Link>
+        <Link href={"/convertor"}>
+          <ToggleGroupItem
+            className="dark:data-[state=on]:bg-[#6161D680] w-1/2 dark:data-[state=off]:bg-[#232336]"
+            value="/convertor"
+          >
+            Convertor
+          </ToggleGroupItem>
+        </Link>
+      </ToggleGroup>
     </div>
   );
 };
