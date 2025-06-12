ee6e9f27fc7b8e88e35d925bb72dc12c86e587c8
daxantz <daxantz@gmail.com>
2025-06-11 22:39:19 -0400

    added coin images to config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
   images: {
+    formats: ["image/avif", "image/webp"],
     domains: ["coin-images.coingecko.com"], // Add the domain here
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "coin-images.coingecko.com",
+        port: "",
+        pathname: "/coins/images/**",
+      },
+    ],
   },
 };
 
